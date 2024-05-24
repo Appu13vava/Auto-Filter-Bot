770334c2e43d61b137a74d27d8ddf70990a0d0ad
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2024-05-24 20:41:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "-1002224975092")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
